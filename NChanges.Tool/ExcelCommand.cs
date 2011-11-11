163a6a919a7c590a2523fb11b4a874f23d8050fb
nikkin <n.nikki@gmail.com>
2011-11-11 15:41:55 -0800

    Added a method to return the keyword for the parameter type.

--- NChanges.Tool/ExcelCommand.cs
+++ NChanges.Tool/ExcelCommand.cs
@@ -93,7 +93,7 @@ namespace NChanges.Tool
                         var methodName = methodInfo.Name;
                         var kind = change.Kind;
                         var version = change.Version;
-                        var parameters = string.Join(", ", methodInfo.Parameters.Select(mi => mi.Type + " " + mi.Name).ToArray());
+                        var parameters = string.Join(", ", methodInfo.Parameters.Select(mi => GetKeywordForTypeName(mi.Type) + " " + mi.Name).ToArray());
                         
                         worksheet.Cells[rowIndex, METHOD_NAME] = new Cell(methodName);
                         worksheet.Cells[rowIndex, NAMESPACE] = new Cell(namesp);
@@ -105,6 +105,44 @@ namespace NChanges.Tool
                         rowIndex++;
                     }
                 }
+            }
+        }
+
+        private string GetKeywordForTypeName(string typeName)
+        {
+            switch (typeName)
+            {
+                case "System.Boolean":
+                    return "bool";
+                case "System.Byte":
+                    return "byte";
+                case "System.SByte":
+                    return "sbyte";
+                case "System.Char":
+                    return "char";
+                case "System.Decimal":
+                    return "decimal";
+                case "System.Double":
+                    return "double";
+                case "System.Single":
+                    return "float";
+                case "System.Int32":
+                    return "int";
+                case "System.UInt32":
+                    return "uint";
+                case "System.Int64":
+                    return "long";
+                case "System.Object":
+                    return "object";
+                case "System.Int16":
+                    return "short";
+                case "System.UInt16":
+                    return "ushort";
+                case "System.String":
+                    return "string";
+
+                default:
+                    return typeName;
             }
         }
 
