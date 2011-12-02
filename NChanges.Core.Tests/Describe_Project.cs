d140a2571332601a2be7bbd3e731fc194aed84fb
Jason Diamond <jason@diamond.name>
2011-12-02 01:02:52 -0800

    Started work on a Project class for the GUI.

---
+++ NChanges.Core.Tests/Describe_Project.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NChanges.Core.Tests
+{
+    [TestFixture]
+    public class Describe_Project
+    {
+        [Test]
+        public void It_writes_XML()
+        {
+            var project = new Project
+            {
+                NChangesToolPath = @"C:\path\to\NChanges.Tool.exe",
+                TypesToExclude = "Internal$",
+                AssembliesToSnapshot =
+                {
+                    new AssemblyToSnapshot { Path = @"C:\path\to\Assembly1.dll" },
+                    new AssemblyToSnapshot { Path = @"C:\path\to\Assembly2.dll", Version = "1.2.3.4" },
+                }
+            };
+
+            var xml = XML.UseWriter(project.WriteXml);
+
+            Assert.AreEqual(
+@"<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <PropertyGroup>
+    <NChangesTool>C:\path\to\NChanges.Tool.exe</NChangesTool>
+    <TypesToExclude>Internal$</TypesToExclude>
+  </PropertyGroup>
+  <ItemGroup>
+    <Assembly Include=""C:\path\to\Assembly1.dll"" />
+    <Assembly Include=""C:\path\to\Assembly2.dll"">
+      <Version>1.2.3.4</Version>
+    </Assembly>
+  </ItemGroup>
+</Project>",
+                xml);
+        }
+    }
+}
---
+++ NChanges.Core/Project.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace NChanges.Core
+{
+    public class Project
+    {
+        private readonly List<AssemblyToSnapshot> _assembliesToSnapshot = new List<AssemblyToSnapshot>();
+
+        public string NChangesToolPath { get; set; }
+        public string TypesToExclude { get; set; }
+        public ICollection<AssemblyToSnapshot> AssembliesToSnapshot { get { return _assembliesToSnapshot; } }
+
+        public void WriteXml(XmlWriter xmlWriter)
+        {
+            xmlWriter.WriteStartElement("Project", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+            WriteProperties(xmlWriter);
+            WriteAssembliesToSnapshot(xmlWriter);
+            WriteTargets(xmlWriter);
+
+            xmlWriter.WriteEndElement();
+        }
+
+        private void WriteProperties(XmlWriter xmlWriter)
+        {
+            xmlWriter.WriteStartElement("PropertyGroup");
+
+            xmlWriter.WriteElementString("NChangesTool", NChangesToolPath);
+            xmlWriter.WriteElementString("TypesToExclude", TypesToExclude);
+
+            xmlWriter.WriteEndElement();
+        }
+
+        private void WriteAssembliesToSnapshot(XmlWriter xmlWriter)
+        {
+            xmlWriter.WriteStartElement("ItemGroup");
+
+            foreach (var ats in AssembliesToSnapshot)
+            {
+                xmlWriter.WriteStartElement("Assembly");
+                xmlWriter.WriteAttributeString("Include", ats.Path);
+
+                if (!string.IsNullOrEmpty(ats.Version))
+                {
+                    xmlWriter.WriteElementString("Version", ats.Version);
+                }
+
+                xmlWriter.WriteEndElement();
+            }
+
+            xmlWriter.WriteEndElement();
+        }
+
+        private void WriteTargets(XmlWriter writer)
+        {
+        }
+    }
+
+    public class AssemblyToSnapshot
+    {
+        public string Path { get; set; }
+        public string Version { get; set; }
+    }
+}
